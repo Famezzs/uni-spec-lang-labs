8fbedcfb73eca44a69466c3beb61e0ab8e4b8c6b
Famezzs <andrii.kozubenko.it.2021@lpnu.ua>
2023-10-11 14:44:26 +0300

    Added scale art function

--- lab3/module/static/MenuFunctions.py
+++ lab3/module/static/MenuFunctions.py
@@ -1,14 +1,27 @@
 from module.InputScanner import InputScanner
 from module.ArtGenerator import ArtGenerator
 from module.ArtPrinter import ArtPrinter
+from module.ArtScaler import ArtScaler
 
 class MenuFunctions:
+    @staticmethod
+    def print_art(result, color):
+        if InputScanner.input_empty(color) == True:
+            ArtPrinter.print(result)
+        else:
+            ArtPrinter.colored_print(result, color)
+
     @staticmethod
     def save_to_file(content):
         path = InputScanner.scan('Specify path to the file: ')
         file = open(path, 'a')
         file.write(content)
         file.close()
+
+    @staticmethod
+    def scale_art(content):
+        multiplier = InputScanner.scan('Specify integer multiplier: ')
+        return ArtScaler.scale(int(multiplier), content)    
 
     @staticmethod
     def text_to_art():
@@ -20,11 +33,13 @@ class MenuFunctions:
             font = ArtGenerator.default_font    
 
         result = ArtGenerator.text_to_art(text=text, font=font)
-        if InputScanner.input_empty(color) == True:
-            ArtPrinter.print(result)
-        else:
-            ArtPrinter.colored_print(result, color)
+        MenuFunctions.print_art(result, color)
         
+        option = InputScanner.scan('Scale art? (Y/N): ')
+        if option.startswith('y'):
+            result = MenuFunctions.scale_art(result)
+            MenuFunctions.print_art(result, color)
+
         option = InputScanner.scan('Save to file? (Y/N): ')
         if option.startswith('y'):
             MenuFunctions.save_to_file(result)
